e04d644a2a7cec243b479f4bd7b4a6b314462133
asiya1330 <asiya.batool987@gmail.com>
2024-06-03 12:05:45 +0500

    add cors

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./config.js/db');
 const userRoutes = require('./routes/user/UserRoute')
+const cors = require('cors')
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 
 // Sync the database
 db.sequelize.sync().then(() => {
   console.log('Database synced');
 });
-
 
 app.use('/api/user', userRoutes);
 
@@ -20,3 +21,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
